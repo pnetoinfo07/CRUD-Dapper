cbfa0f7c0f832d6e080962b272f78f983fc46329
pnetoinfo07 <31972103+netiim@users.noreply.github.com>
2024-09-09 17:08:48 -0300

    dsa

--- CRUD-Dapper/Program.cs
+++ CRUD-Dapper/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 InicializadorBd.Inicializar();
